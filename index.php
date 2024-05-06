7347bc2a0dbe507905a7df31616f7eb45d0c5161
NASIM-PP <nasim@veuz.in>
2024-05-06 17:42:50 +0530

    titile-changed

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Arab Graphics</title>
+    <title>Arab Graphics new</title>
     <link rel="stylesheet" href="assets/css/aos.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
